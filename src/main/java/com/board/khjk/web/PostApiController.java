574beef2d8f240dbdacecdcaa7998d534220b726
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-22 10:00:47 +0900

    Initial commit

---
+++ src/main/java/com/board/khjk/web/PostApiController.java
@@ -0,0 +1,19 @@
+package com.board.khjk.web;
+
+import com.board.khjk.service.PostsService;
+import com.board.khjk.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
