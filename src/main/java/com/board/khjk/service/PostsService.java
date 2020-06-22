e06863f5897e9fdb526cfb37f85ab46c088fe9e2
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-22 14:54:38 +0900

    add BaseTimeEntity, TestCode

--- src/main/java/com/board/khjk/service/PostsService.java
+++ src/main/java/com/board/khjk/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.board.khjk.service;
 
+import com.board.khjk.domain.posts.Posts;
 import com.board.khjk.domain.posts.PostsRepository;
+import com.board.khjk.web.dto.PostsResponseDto;
 import com.board.khjk.web.dto.PostsSaveRequestDto;
+import com.board.khjk.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +19,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+   @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+   }
+
+   @Transactional
+   public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
--- src/main/java/com/board/khjk/web/PostApiController.java
+++ src/main/java/com/board/khjk/web/PostApiController.java
@@ -1,11 +1,11 @@
 package com.board.khjk.web;
 
 import com.board.khjk.service.PostsService;
+import com.board.khjk.web.dto.PostsResponseDto;
 import com.board.khjk.web.dto.PostsSaveRequestDto;
+import com.board.khjk.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,15 @@ public class PostApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
