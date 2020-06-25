4c7f4f16f7ea84a3ff4ff91d72f4a06a1a6680a0
hyojungkang <begywjd@yonsei.ac.kr>
2020-06-25 14:53:09 +0900

    add User entity, OAuth service fix 세션값 가져오는 것 @LoginUser 어노테이션으로 개선하기 (LoginUser, LoginUserArugmentResolver, WebConfig)

--- src/main/java/com/board/khjk/web/IndexController.java
+++ src/main/java/com/board/khjk/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.board.khjk.web;
 
+import com.board.khjk.config.auth.LoginUser;
+import com.board.khjk.config.auth.dto.SessionUser;
 import com.board.khjk.service.PostsService;
 import com.board.khjk.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -15,10 +19,14 @@ import java.util.Map;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user!=null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
