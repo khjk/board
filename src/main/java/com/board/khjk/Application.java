be67655b556795c3dce5fc282cfefdead213f83d
khjk <begywjd@naver.com>
2020-07-11 20:43:12 +0900

    갑자기 왜 travis 일안함?

--- src/main/java/com/board/khjk/Application.java
+++ src/main/java/com/board/khjk/Application.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+        System.out.println("어플리케이션 시작");
     }
 }
